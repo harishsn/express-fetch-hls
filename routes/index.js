93f27c15ba8dbfcd68f8bfa89dc234e40be8784b
harishsn <harishsnsdl@live.in>
2018-02-19 13:53:06 +0530

    Rename

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {
-    title: 'September',
+    title: 'Express fetch hls',
     content: 'App that downloads a playable HLS playlist and associated video segments so that they can be hosted on this server statically for playback.' });
 });
 
