8f6e74b46c33a31dfb739df65ba3911f83890ee6
harishsn <harishsnsdl@live.in>
2018-02-21 15:38:47 +0530

    cleanup

--- utils/db_manager.js
+++ utils/db_manager.js
@@ -1,10 +1,6 @@
 var path = require('path');
 var sqlite3 = require('sqlite3').verbose();
 
-//insertEntry('249414131', 'http://localhost/storage/249414131/249414131.m3u8', 'https://player.vimeo.com/external/249414131.m3u8?s=10bf9d088fff85588fdd56dacd5f9f716c1c8dd5');
-//getAllEntries();
-//hasKey('249414132');
-
 var getEntry = function (remote_url) {
     return new Promise(
         function (resolve, reject) {
